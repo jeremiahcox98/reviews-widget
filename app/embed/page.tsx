ea28f71da9cfa289cdba5d4d6bfd40cc71084d28
Jeremiah Cox <jeremiahc.cox98@gmail.com>
2026-03-07 17:49:28 -0600

    Style

--- app/embed/page.tsx
+++ app/embed/page.tsx
@@ -23,11 +23,11 @@ export default function EmbedPage() {
       {/* 80% black overlay */}
       <div className="absolute inset-0 bg-black/80" aria-hidden />
       {/* Content above overlay */}
-      <div className="relative z-10 py-6">
+      <div className="relative z-10 py-6 pb-10 pt-20">
         <h2
           className={`mb-6 text-center text-[40px] font-medium leading-tight text-white md:text-[50px] ${playfair.className}`}
         >
-          Don&apos;t Take Our Word For It
+          Don&apos;t Just Take Our Word For It
         </h2>
         <ReviewsSection />
       </div>
